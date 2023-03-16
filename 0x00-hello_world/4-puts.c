34dde6e1dc9ca7e56c8ee513ebfe818e8166d9ba
sadson mk <sadsonmkumira@yahoo.com>
2023-03-16 07:17:42 -0700

    Adding the file 4-puts.c.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,13 @@
 
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Outputs text using puts
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("Pogramming is like building a multilingual puzzle");
+	puts("\"Pogramming is like building a multilingual puzzle");
 	return (0);
 }
 
