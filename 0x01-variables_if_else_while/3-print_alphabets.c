30f7f54dd09799c9d2aa34486f3735da750d164e
sadson mk <sadsonmkumira@yahoo.com>
2023-03-17 09:01:53 -0700

    Adding the file 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * while loop - for printing letters of the alphabet
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char lowercase = 'a';
+	char uppercase = 'A';
+
+	while (lowercase <= 'z')
+	{
+		putchar(lowercase);
+
+		lowercase++;
+	}
+
+	while (uppercase <= 'Z')
+	{
+		putchar(uppercase);
+
+		uppercase++;
+	}
+
+	putchar('\n');
+}
