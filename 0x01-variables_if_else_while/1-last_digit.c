91ccd865bcf4b688694094b43729be7ba1d4143f
sadson mk <sadsonmkumira@yahoo.com>
2023-03-17 08:35:56 -0700

    Adding the file 1-last_digit.c

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+/**
+ * main - Entry point
+ * if else statements - check the last digit whether its positive or negative
+ * Return: Always 0 (Success)
+ */
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n, rem;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	remainder = n % 10;
+	if (rem > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5", n, rem);
+	}
+	else if (rem == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, rem);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rem);
+	}
+
+	/* your code goes there */
+	return (0);
+}
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -2,3 +2,5 @@ Learning C-variables, if and else statements, and while loops.
 
 The file 0-positive_or_negative.c is for checking whether a number is negative or positive .
 
+The file 1-last_digit.c is for printing the last digit of the number stored in the variable n.
+
