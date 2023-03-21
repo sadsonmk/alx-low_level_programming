b1d2c19deb235fedba32122961b30a2852522883
sadson mk <sadsonmkumira@yahoo.com>
2023-03-21 11:22:52 -0700

    Adding the file 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,42 @@
 #include "main.h"
 /**
- * times_table - prints the n times table
+ * print_times_table - prints the n times table
  * @n: accepts n as the argument
  * Return: returns nothing
  */
 void print_times_table(int n)
 {
-	int num1, num2, num3, num4, num5;
+	int num1, num2, num3
 
 	if (n >= 0 && n <= 15)
 	{
 	for (num1 = 0; num1 <= n; num1++)
 	{
-		for (num2 = 0; num2 <= n; num2++)
+		_putchar('0');
+		for (num2 = 1; num2 <= n; num2++)
 		{
-			num3 = num1 * num2;
-
-			if (num3 > 9)
-			{
-				num4 = num3 % 10;
-				num5 = (num3 - num4) / 10;
 			_putchar(',');
 			_putchar(' ');
-			_putchar(num5 + '0');
-			_putchar(num4 + '0');
-			}
-			else
+			num3 = num1 * num2
+			if (num3 <= 99)
 			{
-				if (num2 != 0)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-				_putchar(num3 + '0');
+				_putchar(' ');
+			}
+			if (num3 >= 100)
+			{
+				_putchar((num3 / 100) + '0');
+				_putchar(((num3 / 10)) % 10 + '0');
+			}
+
+			else if (num3 <= 99 && num3 >= 10)
+			{
+				_putchar((num3 / 10) + '0');
+				_putchar((num3 % 10) + '0');
 
 			}
-		}
 
 		_putchar('\n');
+		}
 	}
-	}
-	else
-	{
-		return;
 	}
 }
