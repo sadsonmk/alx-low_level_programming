ea87a58425900b977a7a6ca1a950f8de3b6c562b
sadson mk <sadsonmkumira@yahoo.com>
2023-03-21 01:04:16 -0700

    Adding a gitignore file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,12 @@
-#include <unistd.h>
-#include <stdio.h>
+#ifndef MAIN_H
+#define MAIN_H
 
 /**
- * _putchar - writes the character to stdout
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  * Return: On success 1
  * On error -1 is returned
- *
- *
  */
 int _putchar(char c);
+
+#endif
