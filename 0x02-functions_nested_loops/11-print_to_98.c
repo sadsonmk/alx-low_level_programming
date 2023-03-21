a39c71020b33d6e84a0cbe4c5b79941d2024ba5f
sadson mk <sadsonmkumira@yahoo.com>
2023-03-21 10:14:18 -0700

    Adding the file 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -22,6 +22,21 @@ void print_to_98(int n)
 			printf("%d, ", n);
 		}
 		n++;
+
+	}
+	printf("\n");
+
+	while (n >= 98)
+	{
+		if (n == 98)
+		{
+			printf("%d", n);
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
+		n--;
 	}
 
 }
