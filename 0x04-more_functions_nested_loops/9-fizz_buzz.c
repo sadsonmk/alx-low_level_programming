5e9ee973075891deb5f9830201d48bb36f212ba3
sadson mk <sadsonmkumira@yahoo.com>
2023-03-23 10:42:46 -0700

    Adding the file 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = 1;
+
+	while (num <= 100)
+	{
+		if ((num % 3 == 0) && (num % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+		num++;
+	}
+
+	printf("\n");
+	return (0);
+}
