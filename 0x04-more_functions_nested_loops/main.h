1ffec529805836e56fea64d8a3817c1026ed17eb
sadson mk <sadsonmkumira@yahoo.com>
2023-03-23 06:51:46 -0700

    Adding the file 3-print_numbers.c

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -2,6 +2,15 @@
 #define MAIN_H
 
 #include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1
+ * On error -1 is returned
+ */
+int _putchar(char c);
+
 /**
  * _isupper - checks for uppercase character
  * @c: is the character to check
