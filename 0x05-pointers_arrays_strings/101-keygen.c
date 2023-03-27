aaae7ddcb132341799651cc9408ff49825a7947d
sadson mk <sadsonmkumira@yahoo.com>
2023-03-27 11:19:25 -0700

    Adding the file 101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,22 +13,22 @@
 int main(void)
 {
 
-	int password[80];
+	int password[100];
 	int x, y, sum;
 
 	sum = 0;
 
 	srand(time(NULL));
 
-	for (x = 0; x < 80; x++)
+	for (x = 0; x < 100; x++)
 	{
-		password[x] = rand() % 60;
+		password[x] = rand() % 78;
 		sum += (password[x] + '0');
 		putchar(password[x] + '0');
 
-		if ((2000 - sum) - '0' < 60)
+		if ((2772 - sum) - '0' < 78)
 		{
-			y = 2000 - sum - '0';
+			y = 2772 - sum - '0';
 
 			sum += y;
 			putchar(y + '0');
