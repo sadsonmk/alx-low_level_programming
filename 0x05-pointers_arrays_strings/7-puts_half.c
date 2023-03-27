a9051be773bef28434ea10af2ab44e4915aea7ce
sadson mk <sadsonmkumira@yahoo.com>
2023-03-27 08:21:34 -0700

    Adding the file 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,11 +20,11 @@ void puts_half(char *str)
 
 	if (len % 2 == 0)
 	{
-		len2 = len / 2
+		len2 = len / 2;
 	}
 	else
 	{
-		len2 = (len - 1) / 2
+		len2 = (len - 1) / 2;
 	}
 
 	for (i = len2; i <= len; i++)
