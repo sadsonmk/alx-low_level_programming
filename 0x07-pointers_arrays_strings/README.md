a61802cb1e16a5a1c66d512af6ab85080dc41d1d
sadson mk <sadsonmkumira@yahoo.com>
2023-04-02 22:05:48 -0700

    Adding the file 0-memset.c

---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+This is a readme.md for the project 0x07. C - Even more pointers, arrays and strings.
+
+File 0-memset.c is a function that fills memory with a constant byte.
+
+
---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * _memset - fills memory with a constant byte.
+ * @s: pointer to a memory area
+ * @b: a constant byte
+ * @n: number of bytes of memory to be filled
+ * Return: a pointer to the memory area s
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+
+#endif /* MAIN_H */
