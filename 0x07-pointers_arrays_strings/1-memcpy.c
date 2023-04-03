8b98a9b80a0a7112dfb9802042ecbf9dc0e1c69f
sadson mk <sadsonmkumira@yahoo.com>
2023-04-03 06:05:02 -0700

    Adding the file 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,11 +12,13 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	int i;
 
 	i = 0;
 
-	while (n >= i)
+	int x = n;
+
+	while (x > i)
 	{
 		dest[i] = src[i];
 		n--;
