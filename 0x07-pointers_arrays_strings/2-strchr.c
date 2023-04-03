c6e9a41e0c83fb63dcb62fbeabbc8184c71c9e6c
sadson mk <sadsonmkumira@yahoo.com>
2023-04-03 06:50:20 -0700

    Adding the file 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -15,12 +15,12 @@ char *_strchr(char *s, char c)
 
 	i = 0;
 
-	while (s[i] >= '\0')
+	while (s[i] <= '\0')
 	{
-		i++;
+		
 		if (s[i] == c)
 			return (s + i);
-		
+		i++;
 	
 	}
 	return (NULL);
