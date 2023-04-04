b9ce829c83c025e25cf70fcc5788193e1dc1ddc8
sadson mk <sadsonmkumira@yahoo.com>
2023-04-03 18:47:49 -0700

    Adding the file 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,22 +12,26 @@
 void print_diagsums(int *a, int size)
 {
 	int total1, total2;
-
+	
 	int *num;
 
-	int *num2;
+	total1 = 0;
+
+	total2 = 0;
+	
+
 
 	for (num = (a + 0); num <= (a + size); num += size + 1)
 	{
 		total1 += *num;
+		printf("%d\n", *num);
 	}
 
-	for (num2 = (a + 0); num2 <= (a + size - 1); num2 += size - 1)
+	for (num = (a + 0) + 4; num <= (a + size - 1); num += size - 1)
 	{
-		if (num2 == (a + 0))
-			continue;
 
-		total2 += *num2;
+		total2 += *num;
+		printf("%d\n", *num);
 	}
 
 	printf("%d, %d\n", total1, total2);
