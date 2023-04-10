3291daa950d28a232d68adddd90214aaa43a0217
sadson mk <sadsonmkumira@yahoo.com>
2023-04-10 00:50:16 -0700

    Adding the file 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -10,19 +10,11 @@
 int main(int argc, char *argv[])
 {
 	int count_args;
-
-
-	if (argc > 1 && argv)
-	{
-		for(count_args = 1; count_args < argc; count_args++);
-		
-		printf("%d\n", count_args - 1);
-		
-	}
-	else
-	{
-		printf("%d\n", 0);
-	}
+	
+	count_args = 0;
+	while (argc-- > 1 && argv)
+		count_args++;
+	printf("%d\n", count_args++);
 
 	return (0);
 }
