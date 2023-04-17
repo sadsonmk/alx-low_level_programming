56f0aad31f9e3a3d043088e63243fcb9ed46aaf9
sadson mk <sadsonmkumira@yahoo.com>
2023-04-17 11:33:47 -0700

    Adding the struct dog_t in dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -22,4 +22,18 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
+/**
+ * struct dog_t - a new name for the type struct dog.
+ * @name: name of the dog
+ * @owner: owner of the dog
+ * @age: age of the dog
+ */
+
+struct dog_t
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
 #endif /* dog.h */
