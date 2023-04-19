f74618d4bd05e1dabd1c652f17278615e6a03ac3
sadson mk <sadsonmkumira@yahoo.com>
2023-04-19 09:22:34 -0700

    Adding the file 1-array_iterator.c

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,7 +2,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - executes a function given as 
+ * array_iterator - executes a function given as
  * a parameter on each element of an array
  * @array: is the array input
  * @size: are the elements to print
