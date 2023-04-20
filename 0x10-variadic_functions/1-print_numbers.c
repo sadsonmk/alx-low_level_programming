b4971dbadea851704d7c4072a5273b5f5888178f
sadson mk <sadsonmkumira@yahoo.com>
2023-04-20 09:12:10 -0700

    Adding the file 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -25,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		b = va_arg(ap, int);
 		printf("%d", b);
 
-		if (separator != NULL && i != (n - 1)
+		if (separator != NULL && i != (n - 1))
 				printf("%s", separator);
 	}
 
