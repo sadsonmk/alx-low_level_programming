aaca7675bf5e6f95ddc0a74aa4c5bf96ee0d01d2
sadson mk <sadsonmkumira@yahoo.com>
2023-05-01 07:25:22 -0700

    Adding the file 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,25 +1,6 @@
 #include "lists.h"
 
 /**
- * add_data - pushes a new node on the front
- * @head: is a pointer to the head pointer
- * @n: is the data to be added
- * Return: void
- */
-
-void add_data(listint_t **head, int n)
-{
-	listint_t *new_node;
-
-	new_node = malloc(sizeof(listint_t));
-
-	new_node->n = n;
-	new_node->next = (*head);
-	(*head) = new_node;
-
-}
-
-/**
  * insert_nodeint_at_index - inserts a new
  * node at a given position
  * @head: a pointer to the head pointer
@@ -32,27 +13,44 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
 
+	listint_t *new_node;
+
 	listint_t *current = *head;
+
+	new_node = malloc(sizeof(listint_t));
+
+	if (head == NULL || new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
-		add_data(head, n);
-		return (current);
+		new_node->next = (*head);
+		(*head) = new_node;
+		return (new_node);
 	}
 
-	for (i = 0; i < idx - 1; i++)
+	while (current != NULL)
 	{
-		if (current == NULL)
-			return (NULL);
+		for (i = 0; i < idx; i++)
+		{
+			if (i == idx - 1)
+			{
+				new_node->next = current->next;
+				current->next = new_node;
+				return (new_node);
+			}
+			current = current->next;
+		}
 		current = current->next;
 	}
 
-	add_data(&(current->next), n);
-
-
-
-	return (current);
+	return (NULL);
 }
+
+
 
 
 
