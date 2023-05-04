47b88091943f7f988c52e1cf45cfd1238b6f39f0
sadson mk <sadsonmkumira@yahoo.com>
2023-05-04 10:34:04 -0700

    Adding files

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,23 +8,9 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int i, j;
-
-	for (i = 0; i < sizeof(long int); i++)
-	{
-		char byte = ((char *)&n)[i];
-
-		for (j = 8; j > 0; j--)
-		{
-			char bit = byte & 1;
-
-			_putchar(bit + '0');
-			byte >>= 1;
-		}
-	}
-
-
-
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
 
 
