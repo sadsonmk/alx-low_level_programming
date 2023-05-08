eee491a626379974b9ee108e3e9466bdda89524d
sadson mk <sadsonmkumira@yahoo.com>
2023-05-08 12:05:29 -0700

    Adding files

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -19,7 +19,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (file_desc == -1)
 		return (-1);
 
-	if (*text_content != '\0')
+	if (text_content)
 	{
 		for (letters = 0; text_content[letters]; letters++)
 			;
