f9068a9dc7971c15b91a40a3a619d791e1e58b37
sadson mk <sadsonmkumira@yahoo.com>
2023-05-08 11:49:03 -0700

    Adding files

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, "u-x");
+	file_desc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (file_desc == -1)
 		return (-1);
