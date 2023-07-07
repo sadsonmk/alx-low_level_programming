a641ba27654acf44092b5361f45e4b9c92a5024d
Sadsonmk <sadiemcmirror@gmail.com>
2023-07-07 08:46:13 -0700

    Adding the file 2-get_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -28,4 +28,12 @@ unsigned int binary_to_uint(const char *b);
  */
 void print_binary(unsigned long int n);
 
+/**
+ * get_bit - returns the value of a bit at a given index
+ * @n: is the input number
+ * @index: is the index, starting from 0 of the bit you want to get
+ * Return: the value of the bit at index index or -1 if an error occured
+ */
+int get_bit(unsigned long int n, unsigned int index);
+
 #endif /* main.h */
