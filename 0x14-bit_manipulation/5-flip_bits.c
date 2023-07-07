bbb7e4ebde675c1a7fc41b8fbb82fcedc925b26b
Sadsonmk <sadiemcmirror@gmail.com>
2023-07-07 09:31:22 -0700

    Adding the file 5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - the number of bits you would
+ * need to flip to get from one number to another
+ * @n: is an input number
+ * @m: is another input number
+ * Return: number of bits to flip
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, bits = 64, increment = 0;
+	unsigned long int xor, temp;
+
+	xor = n ^ m;
+
+	for (i = bits - 1; i >= 0; i--)
+	{
+		temp = xor >> i;
+
+		if (temp & 1)
+			increment++;
+	}
+	return (increment);
+}
