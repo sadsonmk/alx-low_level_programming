1721716f11f6c3cf87f0a6781b3d2d17033e5cbc
Sadsonmk <sadiemcmirror@gmail.com>
2023-07-10 08:37:52 -0700

    Adding a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 		text_content = "";
-	for (letters = 0; text_content[letters], letters++)
+	for (letters = 0; text_content[letters]; letters++)
 		;
 	file_write = write(file_desc, text_content, letters);
 
