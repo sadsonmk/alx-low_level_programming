f7566c36d14a5e4e9ed54ecde12a870c07431b25
Sadsonmk <sadiemcmirror@gmail.com>
2023-07-10 08:02:04 -0700

    Adding files

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * read_textfile - a function that reads a text file and
+ * prints it to the POSIX standard output
+ * @filename: is the file to be read
+ * @letters: is the number of letters it should read and print
+ * Return: actual number of letters it could read and print
+ * else 0 if the file can not be opened or read
+ * if filename is NULL
+ * if write fails or does not write the expected amount of bytes
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file_read, file_write;
+
+	char *buffer;
+
+	int file_desc;
+
+	if (filename == NULL)
+		return (0);
+	file_desc = open(filename, O_RDONLY);
+	if (file_desc == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+		return (0);
+	file_read = read(file_desc, buffer, letters);
+	file_write = write(STDOUT_FILENO, buffer, file_read);
+
+	close(fiel_desc);
+	free(buffer);
+
+	return (file_write);
+}
+
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,13 @@
+This is the readme for the project 0x15. C - File I/O
+
+1. File 0-read_textfile.c is a function that reads a text file and prints it to the POSIX standard output.
+
+2. File 1-create_file.c is a function that creates a file.
+
+3. File 2-append_text_to_file.c is a function that appends text at the end of a file.
+
+4. File 3-cp.c is a program that copies the content of a file to another file.
+
+5. File 100-elf_header.c is a program that displays the information contained in the ELF header at the start of an ELF file.
+
+
