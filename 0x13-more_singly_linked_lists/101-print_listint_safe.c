48076eaa1b6ce9e4efcea33a8cb18a9463d63d3e
Sadsonmk <sadiemcmirror@gmail.com>
2023-07-29 08:50:13 -0700

    Adding the file

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -2,30 +2,83 @@
 
 
 /**
+ * the_loop_flag - Detects a loop in a linked list.
+ * @head: Pointer to head of the list.
+ * Return: If there is a loop - number of nodes.
+ *	   If there is no loop - 0.
+ */
+size_t the_loop_flag(const listint_t *head)
+{
+	const listint_t *fast, *slow;
+	size_t node = 0;
+
+	if (!head || !head->next)
+		return (0);
+
+	slow = head;
+	fast = head;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				node++;
+				slow = slow->next;
+				fast = fast->next;
+			}
+
+			fast = fast->next;
+			while (slow != fast)
+			{
+				node++;
+				fast = fast->next;
+			}
+			return (++node);
+		}
+	}
+	return (0);
+}
+
+/**
  * print_listint_safe - prints a listint_t linked list
  * @head: pointer to the first node
  * Return: the number of nodes in the list
  */
-
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t nodes = 0;
+	size_t node, i = 0;
 
-	size_t idx = 0;
-	
-	while (head != NULL)
+	if (head == NULL)
+		exit(98);
+
+	node = loop_flag(head);
+
+	if (node == 0)
 	{
-		printf("[%p] %d\n", (void *)head, head->n);
-		nodes += 1;
-		head = head->next;
+		while (head)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			node++;
+			head = head->next;
+		}
 	}
 
-	for (idx = 0; idx < nodes; idx++)
+	else
 	{
+		while (i < node)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			i++;
+		}
 		printf("-> [%p] %d\n", (void *)head, head->n);
-		
 	}
-	
-	return (nodes);
-}
 
+	return (node);
+}
