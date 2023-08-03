a4c0f0235211c5e34420f47c630f269456856d41
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-03 07:16:40 -0700

    Adding the file to exercise 5

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,6 +9,13 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	return (n & (~(1 << (index - 1))));
+	int new_value;
+
+	if (index > 63)
+		return (-1);
+
+	new_value = (n & (~(1 << (index - 1))));
+
+	return (new_value);
 }
 
