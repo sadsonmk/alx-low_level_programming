8af0d310c1dc09be37d59c70b633037b701b5a89
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-17 11:15:55 -0700

    Adding files

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -4,7 +4,7 @@
  * get_dnodeint_at_index - the nth node of a dlistint_t linked list
  * @head: is the head pointer
  * @index: is the node you have to return
- * Return: a pointer to the item on the specified index 
+ * Return: a pointer to the item on the specified index
  * or NULL if its non-existent
  */
 
