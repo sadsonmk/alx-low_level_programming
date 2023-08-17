cdfdd92dedafc60626edf8cf76a9f11f669d36ac
Sadson Mk <sadsonmkumira@yahoo.com>
2023-08-17 12:04:02 -0700

    Adding files

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,11 +17,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 
-	if (idx < b)
-		return (NULL);
-
-
 	if (!new_node)
+		return (NULL);
+	if (idx < b)
 		return (NULL);
 
 	new_node->n = n;
