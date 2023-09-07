fbfaa79353c5b93cf7725caee02f0284c8a35dc1
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-07 09:49:29 -0700

    Adding files

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,21 +9,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
-	hash_table_t *h_table = (hash_table_t *) malloc(sizeof(hash_table_t));
+	hash_table_t *table = (hash_table_t *) malloc(sizeof(hash_table_t));
 
-	if (!h_table)
+	if (!table)
 		return (NULL);
 
-	h_table->size = size;
-	h_table->array = (hash_node_t **) calloc(h_table->size, sizeof(hash_node_t *));
+	table->size = size;
+	table->array = (hash_node_t **) calloc(table->size, sizeof(hash_node_t *));
 
-	if (!h_table->array)
+	if (!table->array)
 		return (NULL);
-	for (i = 0; i < h_table->size; i++)
+	for (i = 0; i < table->size; i++)
 	{
-		h_table->array[i] = NULL;
+		table->array[i] = NULL;
 	}
 
 
-	return (h_table);
+	return (table);
 }
