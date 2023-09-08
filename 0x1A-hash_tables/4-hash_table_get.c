858ed5f6a994277082e697eb9d0d47d37bd4dc3b
Sadson Mk <sadsonmkumira@yahoo.com>
2023-09-08 04:34:50 -0700

    Adding files

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,6 +12,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *item;
 
+	if (!ht || !key)
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	item = ht->array[index];
