805ae494419f0a411937a95bf5cab0cbe1d1cbbd
Sadson <sadsonmkumira@yahoo.com>
2023-12-12 14:05:22 +0000

    Adding first files

---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+This is the readme file for the project 0x1E-search_algorithms
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,10 @@
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+/*standard library */
+#include <stdio.h>
+
+/* prototypes */
+int linear_search(int *array, size_t size, int value);
+
+#endif /* search_algos header file */
