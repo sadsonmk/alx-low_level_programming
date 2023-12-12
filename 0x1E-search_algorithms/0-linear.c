a3ed261fa6697c3662f4001a0f6370c18cf34b80
Sadson <sadsonmkumira@yahoo.com>
2023-12-12 14:12:01 +0000

    Adding first files

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -21,7 +21,7 @@ int linear_search(int *array, size_t size, int value)
 		if (array[i] == value)
 		{
 			printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-			return (array[i]);
+			return (i);
 		}
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 	}
